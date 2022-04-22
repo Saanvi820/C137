f404cc0c295304f86be4522016536aefe15efbb6
Saanvi820 <76711372+Saanvi820@users.noreply.github.com>
2022-04-21 20:32:30 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+status = "";
+objects = [];
+function setup() {
+    canvas = createCanvas(440, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(440, 300);
+    video.hide();
+}
+function draw() {
+    image(video, 0, 0, 440, 300);
+    if (status != "") {
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Objects Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of Objects detected are: " + objects.length;
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + "" + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height)
+        }
+    }
+}
+function start() {
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status :Detecting Objects";
+}
+function modelLoaded() {
+    console.log("Model Loaded.");
+    status = true;
+}
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
